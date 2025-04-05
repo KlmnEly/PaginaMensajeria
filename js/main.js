f3ddc161f536c74986dcaf59adb75bbfbe4ce022
Kelmin Miranda <kelminemh@hotmail.com>
2025-04-05 15:42:44 -0500

    Nuevas Paginas, trabajo finalizado

--- js/main.js
+++ js/main.js
@@ -3,29 +3,43 @@ const inputSeleccion = document.getElementById('inputSeleccion');
 const menu = document.querySelector('.menu');
 const navLinks = document.querySelector('.nav-links');
 
-seleccionConsulta.addEventListener('change', function () {
-    const opcionSeleccionada = this.value;
-    let nuevoPlaceholder = '';
+if (seleccionConsulta) {
+    seleccionConsulta.addEventListener('change', function () {
+        const opcionSeleccionada = this.value;
+        let nuevoPlaceholder = '';
 
-    switch (opcionSeleccionada) {
-        case "envios":
-            nuevoPlaceholder = "Ingresa el # de guia"
-            break;
+        switch (opcionSeleccionada) {
+            case "envios":
+                nuevoPlaceholder = "Ingresa el # de guia"
+                break;
 
-        case "remision":
-            nuevoPlaceholder = "Ingresa el # de remosion"
-            break;
-        case "factura":
-            nuevoPlaceholder = "Ingresa el # de factura"
-            break;
-        case "recoleccion":
-            nuevoPlaceholder = "Ingresa el codigo"
-            break;
-    }
+            case "remision":
+                nuevoPlaceholder = "Ingresa el # de remosion"
+                break;
+            case "factura":
+                nuevoPlaceholder = "Ingresa el # de factura"
+                break;
+            case "recoleccion":
+                nuevoPlaceholder = "Ingresa el codigo"
+                break;
+        }
 
-    inputSeleccion.placeholder = nuevoPlaceholder;
-})
+        inputSeleccion.placeholder = nuevoPlaceholder;
+    })
+}
 
-menu.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-})
+if (menu) {
+
+    menu.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    })
+}
+
+let map;
+
+function initMap () {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+    });
+}
